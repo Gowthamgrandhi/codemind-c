59b105d4cf49c44178bed4a7e8043a83e399074b
Gowthamgrandhi <97425819+Gowthamgrandhi@users.noreply.github.com>
2022-05-07 11:56:08 +0530

    #include<stdio.h> int main() {     int a,b,c,i;     scanf("%d %d",&a,&b);     for(i=1;i<=a*b;i++)     {         if(i%a==0 && i%b==0)         {             printf("%d",i);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -3,21 +3,12 @@ int main()
 {
     int a,b,c,i;
     scanf("%d %d",&a,&b);
-    if(a>b)
-    {
-        c=a;
-    }
-    else
-    {
-        c=b;
-    }
-    for(i=1;i<=c;i++)
+    for(i=1;i<=a*b;i++)
     {
         if(i%a==0 && i%b==0)
         {
             printf("%d",i);
             break;
         }
-        ++c;
     }
 }
