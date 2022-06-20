40228265ff7023b4834973b41e631897f24e6c15
Gowthamgrandhi <97425819+Gowthamgrandhi@users.noreply.github.com>
2022-06-20 15:28:16 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==1||i==n||i==j)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,18 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
+            if(j==1||i==n||i==j)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
